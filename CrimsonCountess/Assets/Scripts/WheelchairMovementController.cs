f1a3700a77cd626bc77cd911d2406507c0f08702
BlakeShortland <40289871+BlakeShortland@users.noreply.github.com>
2019-11-20 04:15:03 +0800

    Commented Scripts, increased strength of haptics

--- CrimsonCountess/Assets/Scripts/WheelchairMovementController.cs
+++ CrimsonCountess/Assets/Scripts/WheelchairMovementController.cs
@@ -11,18 +11,21 @@ public class WheelchairMovementController : MonoBehaviour
     [SerializeField] VRTK_ArtificialRotator leftWheel;
     [SerializeField] VRTK_ArtificialRotator rightWheel;
 
+    //Keeps track of what angle the wheel was in the last frame
     float prevLeftWheelAngle;
     float prevRightWheelAngle;
 
+    //Reprisents the difference in angle between frames divided by the time between frames.
     float leftWheelSpeed;
     float rightWheelSpeed;
 
-    float rotationSmoothing = 5f;
+    //These values make the wheelchair controllable
     float speedReducer = 0.005f;
     float speedIncreaser = 10f;
 
     void Start()
     {
+        //Set the "previous" rotation values to avoid null references
         prevLeftWheelAngle = leftWheel.GetValue();
         prevRightWheelAngle = rightWheel.GetValue();
     }
@@ -36,20 +39,25 @@ public class WheelchairMovementController : MonoBehaviour
 
     void CalculateWheelSpeed()
     {
+        //Calculate the speed of each wheel
         leftWheelSpeed = (leftWheel.GetValue() - prevLeftWheelAngle) / Time.deltaTime * speedReducer;
         rightWheelSpeed = (rightWheel.GetValue() - prevRightWheelAngle) / Time.deltaTime * speedReducer;
 
+        //Save the angle of rotation for the next frame's calculation
         prevLeftWheelAngle = leftWheel.GetValue();
         prevRightWheelAngle = rightWheel.GetValue();
     }
 
+    //Determines how fast forwards/backwards to move the wheelchair
     void Move()
     {
+        //Forward speed is the average speed of both wheels. This prevents super speed when the wheels are just added together.
         float forwardSpeed = (leftWheelSpeed + rightWheelSpeed) / 2;
 
         transform.Translate(Vector3.forward * forwardSpeed * Time.deltaTime);
     }
 
+    //Determines which wheel is spinning faster and rotates the wheelchair accordingly
     void Rotate()
     {
         float rotateSpeed = leftWheelSpeed - rightWheelSpeed;
