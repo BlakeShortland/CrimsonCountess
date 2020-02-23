dea75623c4899de1a5100775bcdb18def74ce804
BlakeShortland <40289871+BlakeShortland@users.noreply.github.com>
2020-02-23 10:23:38 +0800

    23/02/20
    + Ritual item prefabs
    + Converted furniture to prefabs for reuse
    + Create blood spurt effect

---
+++ CrimsonCountess/Assets/Scripts/KnifeItem.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnifeItem : MonoBehaviour
+{
+	[SerializeField] GameObject bloodSpurt;
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		if(collision.gameObject.tag == "RightController" || collision.gameObject.tag == "LeftController")
+		{
+			ContactPoint firstContactPoint = collision.contacts[0];
+			GameObject bloodSpurtFX = Instantiate(bloodSpurt, firstContactPoint.point, Quaternion.identity);
+			bloodSpurtFX.transform.up = firstContactPoint.normal;
+		}
+	}
+}
