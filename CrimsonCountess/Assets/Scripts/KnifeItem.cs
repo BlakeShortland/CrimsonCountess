449bcf6ff91587d3afcbcec56a4b51563d3fd2aa
BlakeShortland <40289871+BlakeShortland@users.noreply.github.com>
2020-02-25 09:24:25 +0800

    25/02/20
    + Painting
    + Edited colliders
    + Worked on knife cutting
    + Addeed roof

--- CrimsonCountess/Assets/Scripts/KnifeItem.cs
+++ CrimsonCountess/Assets/Scripts/KnifeItem.cs
@@ -8,7 +8,7 @@ public class KnifeItem : MonoBehaviour
 
 	private void OnCollisionEnter(Collision collision)
 	{
-		if(collision.gameObject.tag == "RightController" || collision.gameObject.tag == "LeftController")
+		if(collision.gameObject.tag == "RightHand" || collision.gameObject.tag == "LeftHand")
 		{
 			ContactPoint firstContactPoint = collision.contacts[0];
 			GameObject bloodSpurtFX = Instantiate(bloodSpurt, firstContactPoint.point, Quaternion.identity);
