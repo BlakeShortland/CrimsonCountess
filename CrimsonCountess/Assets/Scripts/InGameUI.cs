cdda483ecf66cbf06487e8e8ccf5513601deb930
BlakeShortland <40289871+BlakeShortland@users.noreply.github.com>
2020-04-24 18:32:51 +0800

    24/04/20
    UI stuff

---
+++ CrimsonCountess/Assets/Scripts/InGameUI.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class InGameUI : MonoBehaviour
+{
+	public GameObject mainMenuCanvas;
+
+    public void ReturnToMenu()
+	{
+		SceneManager.LoadScene("MainMenu");
+	}
+
+	public void ToggleMenu()
+	{
+		if (mainMenuCanvas.activeInHierarchy)
+			HideMenu();
+		else
+			ShowMenu();
+	}
+
+	void ShowMenu()
+	{
+		mainMenuCanvas.SetActive(true);
+	}
+
+	void HideMenu()
+	{
+		mainMenuCanvas.SetActive(false);
+	}
+}
---
+++ CrimsonCountess/Assets/Scripts/MoveOnLoad.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveOnLoad : MonoBehaviour
+{
+	[SerializeField] Vector3 startPos;
+
+    void Start()
+    {
+		transform.localPosition = startPos;
+    }
+}
