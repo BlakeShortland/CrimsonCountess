10f955a7322424473358e11fd99cd869dd2e873e
BlakeShortland <40289871+BlakeShortland@users.noreply.github.com>
2020-04-27 18:09:30 +0800

    27/04/20

---
+++ CrimsonCountess/Assets/Scripts/OtherPaintingController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OtherPaintingController : MonoBehaviour
+{
+	GameController gameController;
+
+	Renderer paintingRenderer;
+	public Texture[] paintingStages = new Texture[3];
+
+	void Start()
+    {
+		gameController = GameController.Instance;
+
+		paintingRenderer = GetComponent<Renderer>();
+
+		paintingRenderer.material.mainTexture = paintingStages[0];
+
+		gameController.candleGrab.AddListener(SecondStage);
+		gameController.bookPickedUp.AddListener(ThirdStage);
+	}
+
+    void SecondStage()
+	{
+		paintingRenderer.material.mainTexture = paintingStages[1];
+	}
+
+	void ThirdStage()
+	{
+		paintingRenderer.material.mainTexture = paintingStages[2];
+	}
+}
