2d34ac2047826e51d24782266a63ee445b66302c
James Noonan <warped.demon.prince@hotmail.com>
2020-05-01 03:52:07 +0800

    Light Flickering Added

--- CrimsonCountess/Assets/Scripts/CeilingLightController.cs
+++ CrimsonCountess/Assets/Scripts/CeilingLightController.cs
@@ -10,6 +10,13 @@ public class CeilingLightController : MonoBehaviour
 	AuraLight auraLight;
 	GameController gameController;
 
+    bool isFlickering = false;
+
+    float flickerTimer = 0f;
+    float flickerDelay = .1f;
+    float flickerFuse = 1f;
+
+
 	void Start()
 	{
 		gameController = GameController.Instance;
@@ -17,9 +24,45 @@ public class CeilingLightController : MonoBehaviour
 		myLight = GetComponent<Light>();
 		gameController.candleGrab.AddListener(DisableLight);
 		gameController.candlePlace.AddListener(EnableLight);
-	}
 
-	public void EnableLight()
+    }
+
+    public void StartFlicker()
+    {
+        isFlickering = true;
+
+        flickerTimer = Random.Range(0f,1f);
+    }
+
+    public void StopFlicker()
+    {
+        isFlickering = false;
+    }
+
+    private void Update()
+    {
+        if (isFlickering)
+        {
+            flickerTimer += Time.deltaTime;
+            if (flickerTimer >= flickerFuse)
+            {
+                float random = Random.Range(0, 100);
+                if (random <= 5)
+                {
+                    Flicker();
+                }
+                flickerTimer = 0f;
+            }
+        }
+    }
+
+    public void Flicker()
+    {
+        DisableLight();
+        Invoke("EnableLight", flickerDelay);
+    }
+
+    public void EnableLight()
 	{
 		auraLight.enabled = true;
 		myLight.enabled = true;
@@ -30,4 +73,6 @@ public class CeilingLightController : MonoBehaviour
 		auraLight.enabled = false;
 		myLight.enabled = false;
 	}
+
+
 }
